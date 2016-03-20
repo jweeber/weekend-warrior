0fd26ad964ac9d42cdb561c6acad33d75071323c
Jessica <jessica.weeber@gmail.com>
2016-03-20 15:02:33 -0700

    created better variable names

--- 03-hamming/hamming.rb
+++ 03-hamming/hamming.rb
@@ -1,18 +1,18 @@
 class Hamming
 
-  def self.compute(string1, string2)
-    raise ArgumentError if string1.length != string2.length
+  def self.compute(strand1, strand2)
+    raise ArgumentError if strand1.length != strand2.length
 
-    string1 = string1.split(//)
-    string2 = string2.split(//)
+    strand1 = strand1.split(//)
+    strand2 = strand2.split(//)
 
-    strands = string1.zip(string2)
+    complete_strands = strand1.zip(strand2)
 
     distance = 0
-    strands.each do |strand|
+    complete_strands.each do |strand|
       distance += 1 if strand[0] != strand[1]
     end
-    
+
     return distance
   end
 
