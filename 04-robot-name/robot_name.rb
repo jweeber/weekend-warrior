dfb64c93d2c25eef43eaf9fd4442ce1473a8478c
Jessica <jessica.weeber@gmail.com>
2016-03-20 16:33:24 -0700

    robot names can generate and tests passing

---
+++ 04-robot-name/robot_name.rb
@@ -0,0 +1,15 @@
+class Robot
+  
+  LTR_ARRAY = ('A'..'Z').to_a
+  NUM_ARRAY = (0..9).to_a
+
+  attr_reader :name
+
+  def initialize
+    @name = LTR_ARRAY.sample(2).join + NUM_ARRAY.sample(3).join
+  end
+
+  def reset
+    @name = LTR_ARRAY.sample(2).join + NUM_ARRAY.sample(3).join
+  end
+end
