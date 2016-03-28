feeda20b425f5910152e581ef2e966b11feef2de
Jessica <jessica.weeber@gmail.com>
2016-03-28 10:29:52 -0700

    comment and spacing

--- 04-robot-name/robot_name.rb
+++ 04-robot-name/robot_name.rb
@@ -1,15 +1,17 @@
 class Robot
-  
+
   LTR_ARRAY = ('A'..'Z').to_a
   NUM_ARRAY = (0..9).to_a
 
   attr_reader :name
 
   def initialize
+    # reset
     @name = LTR_ARRAY.sample(2).join + NUM_ARRAY.sample(3).join
   end
 
   def reset
     @name = LTR_ARRAY.sample(2).join + NUM_ARRAY.sample(3).join
   end
+
 end
