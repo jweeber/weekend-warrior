5d39aa94f10f84530c82f8577fb18322ba62a490
Jessica <jessica.weeber@gmail.com>
2016-04-15 11:59:37 -0700

    finished and all tests passing, but want to see if i can come back and refactor with enumerables or regex later

--- 06-rna-transcription/rna_transcription.rb
+++ 06-rna-transcription/rna_transcription.rb
@@ -1,11 +1,42 @@
+
 class Complement
-	def of_dna(nucleotide)
-		dna_strand = nucleotide.split(//)
-		
-		
+
+	def self.of_dna(dna_strand)
+
+		rna = []
+		dna_strand.split(//).each do |nucleotide|
+			if nucleotide.match("A")
+				rna << "U"
+			elsif nucleotide.match("T")
+				rna << "A"
+			elsif nucleotide.match("C")
+				rna << "G"
+			elsif nucleotide.match("G")
+				rna << "C"
+			else
+				raise ArgumentError
+			end
+		end
+		return rna.join
 	end
 
+	def self.of_rna(rna_strand)
 
+	dna = []
+	rna_strand.split(//).each do |nucleotide|
+		if nucleotide.match("A")
+			dna << "T"
+		elsif nucleotide.match("U")
+			dna << "A"
+		elsif nucleotide.match("C")
+			dna << "G"
+		elsif nucleotide.match("G")
+			dna << "C"
+		else
+			raise ArgumentError
+		end
+	end
+	return dna.join
 end
 
-Complement.of_dna('C')
+end
